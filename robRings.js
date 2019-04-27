12e7062045151c364a78e4da56d82879c8b9db8b
Jasmyne Boggs <jrbbp7@mail.missouri.edu>
2019-04-27 17:33:58 -0500

    robsrings

--- robRings.js
+++ robRings.js
@@ -1,44 +1,13 @@
-/*
-count = 0;
-document.getElementById('btn').addEventListener('click', function() { 
-    
-    count += 1;
-    var totalQty = document.querySelector('.quantity__1');
-    totalQty.textContent = count; 
-});
+document.getElementById("add__item").addEventListener("click", myFunction);
 
-document.getElementById('btn1').addEventListener('click', function() { 
-    
-    count += 1;
-      var totalQty = document.querySelector('.quantity__2');
-      totalQty.textContent = count; 
-  });
-
-  document.getElementById('btn2').addEventListener('click', function() { 
-    
-    count += 1;
-      var totalQty = document.querySelector('.quantity__3');
-      totalQty.textContent = count; 
-  });
-  document.getElementById('btn3').addEventListener('click', function() { 
-    
-     count += 1;
-      var totalQty = document.querySelector('.quantity__4');
-      totalQty.textContent = count; 
-  });
-  */
-
-var DOMstrings = {
-    inputType: '.add__type',
-    inputValue: '.add__value'
-};
-
-document.getElementById('btn').addEventListener('click', function() { 
-    // var typeInput = document.querySelector(DOMstrings.inputType).value; // either subtract or add to inventory
-    var value = parseFloat(document.querySelector(DOMstrings.inputValue).value);
-    
-    var totalQty = document.querySelector('.quantity__1');
-    totalQty.textContent = value;
-
-});
-
+function myFunction() {
+  let grabTable = document.getElementById("main__table");
+  let row = grabTable.insertRow(1);
+  let newCell1 = row.insertCell(0);
+  let newCell2 = row.insertCell(1);
+  let newCell3 = row.insertCell(2);
+  let newCell4 = row.insertCell(3);
+  let newCell5 = row.insertCell(4);
+  let newCell6 = row.insertCell(5);
+  let newCell7 = row.insertCell(6);
+}
