ba4a92d6d2a369daba656209839f27f428d22a03
Jasmyne Boggs <Jasmyne.Boggs@vu.com>
2019-05-10 15:48:17 -0500

    now can add multiple rows w/ dif info

--- robRings.js
+++ robRings.js
@@ -1,45 +1,60 @@
 function addNewItem() {
-  let table = document.getElementById("main_table");
-  // creates new row
-  let row = table.insertRow(1);
-  
-  // creates cells in new row
-  let cellSku = row.insertCell(0);
-  let cellDescription = row.insertCell(1);
-  let cellQty = row.insertCell(2)
-  let cellCost = row.insertCell(3)
-  let cellPrice = row.insertCell(4)
-  let cellImage = row.insertCell(5)
-  
-  document.querySelector('.bg-modal').style.display = 'flex';
 
-  document.getElementById('btn-add-new').addEventListener('click', function(){
-    //  closes modal when submit button is clicked
-    document.querySelector('.bg-modal').style.display = 'none';
+let cellSku, cellDescription, cellQty, cellCost, cellPrice, cellImage;
+
+document.querySelector('.bg-modal').style.display = 'flex';
+
+
+document.getElementById('btn-add-new').addEventListener('click', function() {
+
+  // closes modal when submit button is clicked
+ document.querySelector('.bg-modal').style.display = 'none';
+
+  // updates user interface
+
+  if (document.getElementById("new_sku").value > 0) {
+      let table = document.getElementById("main_table");
+    
+      // creates new row
+      let row = table.insertRow(1);
   
-    // updates user interface
-    cellSku.innerHTML = document.getElementById("new_sku").value;
-    cellDescription.innerHTML = document.getElementById("new_descrip").value;
-    cellQty.innerHTML = document.getElementById("new_qty").value;
-    cellCost.innerHTML = document.getElementById("new_cost").value;
-    cellPrice.innerHTML = document.getElementById("new_price").value;
+      // creates cells in new row
+      cellSku = row.insertCell(0);
+      cellDescription = row.insertCell(1);
+      cellQty = row.insertCell(2)
+      cellCost = row.insertCell(3)
+      cellPrice = row.insertCell(4)
+      cellImage = row.insertCell(5)
+
+      // updates user interface
+      cellSku.innerHTML = document.getElementById("new_sku").value;
+      cellDescription.innerHTML = document.getElementById("new_descrip").value;
+      cellQty.innerHTML = document.getElementById("new_qty").value; 
+      cellCost.innerHTML = "$" + document.getElementById("new_cost").value;
+      cellPrice.innerHTML = "$" + document.getElementById("new_price").value;
     
-    // upload img
-    picture = document.getElementById("file").files[0].name;
-    cellImage.innerHTML = '<img src="' + picture + '" width="200" height="200" alt="skull ring">'
+      // upload img
+      picture = document.getElementById("file").files[0].name;
+      cellImage.innerHTML = '<img src="' + picture + '" width="200" height="200" alt="skull ring">'
     
+    } else if (document.getElementById("new_sku").value <= 0) {
+      alert('Enter Sku Number');
+    }
+
+  // clear fields once you hit submit
+  document.getElementById('new_sku').value = '';
+  document.getElementById('new_descrip').value = '';
+  document.getElementById('new_qty').value = '';
+  document.getElementById('new_cost').value = '';
+  document.getElementById('new_price').value = '';
+  document.getElementById('file').value = '';
+
   
   });
-
 }
- 
+
 // close modal
 document.querySelector('.close').addEventListener('click', function() {
   document.querySelector('.bg-modal').style.display = 'none';
 });
-
-
-
-
-
 
