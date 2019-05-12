c12d31a921d7a4f0928e5671a1e6242037c0d595
UnitedstateofDakota <43390951+UnitedstateofDakota@users.noreply.github.com>
2019-05-12 17:57:19 -0500

    add to table got it

--- robRings.js
+++ robRings.js
@@ -11,18 +11,30 @@ function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
 
-function submitForm() {
+function submitForm(sku, description, quantity, cost, price, image, quantity)
+//add 7 cells in new row
+{   //where are we putting it
     let table = document.getElementById("main_table");
+    //making new row
     let row = table.insertRow(1);
+    //add info from Sku text box
     let cellsku = row.insertCell(0); 
-    console.log(row);
-    cellsku.innerHTML = document.getElementById("sku").value;
-    let description = document.getElementById("description").value;
-    let quantity = document.getElementById("quantity").value;
-    let cost = document.getElementById("cost").value;
-    let price = document.getElementById("price").value;
-    let image = document.getElementById("file").value;
-    }
+        cellsku.innerHTML = document.getElementById("sku").value;
+    //add info from description 
+    let celldescription = row.insertCell(1); 
+    celldescription.innerHTML = document.getElementById("description").value;
+    let cellquantity = row.insertCell(2); 
+        cellquantity.innerHTML = document.getElementById("quantity").value;
+    let cellcost = row.insertCell(3); 
+        cellcost.innerHTML = document.getElementById("cost").value;
+    let cellprice = row.insertCell(4); 
+        cellprice.innerHTML = document.getElementById("price").value;
+    let cellimage = row.insertCell(5); 
+    cellimage.innerHTML = document.getElementById("file").value;
+    //add info in quantity field *next step can edit and add too others
+    let cellamount = row.insertCell(6);
+    cellamount.innerHTML = document.getElementById("quantity").value;
+ }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
