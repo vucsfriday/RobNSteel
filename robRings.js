939bacff9969f9b787fc5f4e4663ee16d8ec8a87
Dakota Jones <Dakota.Jones@vu.com>
2019-05-19 17:58:55 -0500

    Add image to inventory

--- robRings.js
+++ robRings.js
@@ -8,7 +8,7 @@ document.getElementById('btn').addEventListener('click', function() {
 
 });
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
+  document.getElementById("myModal").style.display = "block";
 }
 
 function submitForm(sku, description, quantity, cost, price, image, quantity)
@@ -29,12 +29,17 @@ function submitForm(sku, description, quantity, cost, price, image, quantity)
         cellcost.innerHTML = document.getElementById("cost").value;
     let cellprice = row.insertCell(4); 
         cellprice.innerHTML = document.getElementById("price").value;
-    let cellimage = row.insertCell(5); 
-    cellimage.innerHTML = document.getElementById("file").value;
+    let cellImage = row.insertCell(5); 
+    cellImage.innerHTML = document.getElementById("file").value;
     //add info in quantity field *next step can edit and add too others
     let cellamount = row.insertCell(6);
     cellamount.innerHTML = document.getElementById("quantity").value;
- }
+
+    let picture = document.getElementById("file").files[0].name;
+    console.log(picture);
+    cellImage.innerHTML = '<img src="' + picture + '" width="200" height="200" alt="skull ring">'
+
+}
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
